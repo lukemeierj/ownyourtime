88809d13eab2d2e67a8a7cfad835f71ff6b533c9
Luke Meier <lukemeierj@gmail.com>
2017-08-06 08:45:50 -0400

    Add min word count & shake effect

--- catcher.js
+++ catcher.js
@@ -1,4 +1,4 @@
-var wordlen = 15;
+var wordlen = 7;
 
 $('#explain').bind("paste",function(e) {
 	e.preventDefault();
@@ -9,11 +9,13 @@ $(document).on('keyup', function(e){
 		var url = window.location.href;
 		var uri = new URI(url),
             queryObj = URI.parseQuery(uri.query()),
-            words = $("#explain").val();
-		if(!Gibberish.isGibberish(words)){
+            explain  = $("#explain")
+            words    = explain.val();
+		if(!Gibberish.isGibberish(words) && words.length > wordlen){
 			chrome.runtime.sendMessage({redirect: queryObj['next']});
-
-		} 
+		} else {
+			$("#explain").shake({speed: 40, distance: 8, times: 2}).val('');
+		}
 	}
 
 })
